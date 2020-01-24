05f3e4158219c341b6ba731659603a3dd207c452
Dani Matton <danielle@Danis-MacBook-Pro.local>
2020-01-24 00:12:12 -0700

    NYB and TOI news headlines

--- core/views.py
+++ core/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render, redirect
+from .models import Headline
+from .models import Book
+from django.core.files.storage import FileSystemStorage
+from .forms import BookForm
 # auth imports
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
@@ -8,13 +12,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 # scraping imports
 import requests
 from bs4 import BeautifulSoup
-from .models import Headline
-from django.shortcuts import render
-from django.views.generic import TemplateView
-from django.core.files.storage import FileSystemStorage
 
-from .forms import BookForm
-from .models import Book
+
+# book app
 
 class Home(TemplateView):
     template_name = 'home.html'
@@ -52,6 +52,24 @@ def delete_book(request, pk):
         book.delete()
     return redirect('book_list')
 
+# scraper app
+
+def index(request):
+    context = {'toi_news':toi_news, 'nyb_news':nyb_news}
+    return render(request, 'home.html', context)
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {
+        'form': form
+    })
+
 ## india times
 toi_r = requests.get("https://timesofindia.indiatimes.com/briefs")
 toi_soup = BeautifulSoup(toi_r.content, 'html.parser')
@@ -66,24 +84,20 @@ for th in toi_headings:
 
 #nyb
 
+nyb_r = requests.get("https://www.nytimes.com/column/bits")
+nyb_soup = BeautifulSoup(nyb_r.content, 'html.parser')
+
+posts = nyb_soup.find_all('div', {'class':'css-1l4spti'})
+
+nyb_news = []
+for i in posts:
+    nyb_news.append(i.text)
 
 
+# for i in posts:
+#     nyb_news.append(title)
+#     nyb_news.append(link)
 
-def index(request):
-    context = {'toi_news':toi_news}
-    return render(request, 'home.html', context)
-
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    else:
-        form = UserCreationForm()
-    return render(request, 'registration/signup.html', {
-        'form': form
-    })
 
 # def news_list(request):
 #     headlines = Headline.objects.all()
@@ -134,35 +148,9 @@ def signup(request):
 #     nyb_news.append(title)
 #     nyb_news.append(link)
 
-
-nyb_r = requests.get("https://www.nytimes.com/column/bits")
-nyb_soup = BeautifulSoup(nyb_r.content, 'html.parser')
-
-posts = nyb_soup.find_all('div', {'class':'css-1l4spti'})
-
-for i in posts:
-    link = i.find_all('a', {'href': True})[0]
-    title = i.find('h2', {'class':'css-1j9dxys'}).text
-
-nyb_news = []
-
-for i in posts:
-    nyb_news.append(title)
-    nyb_news.append(link)
-
-
-
-
-
-
-
 # @login_required
 # def secret_page(request):
 #     return render(request, 'secret_page.html')
 
 # class SecretPage(LoginRequiredMixin, TemplateView):
 #     template_name = 'secret_page.html'
-
-# def index(request):
-#     context = {'toi_news':toi_news}
-#     return render(request, 'home.html', context)
